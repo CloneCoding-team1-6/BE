77664a6f26d132f441f73ea3ca8440eff890c3e2
Bongjin Choi <cbjjzzang@naver.com>
2022-04-15 10:32:59 +0900

    dd

---
+++ src/main/java/com/sparta/cloneback/CloneBackApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.cloneback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/cloneback/CloneBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.cloneback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
